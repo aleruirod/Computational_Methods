b947d80e1d53ad3feb0f1e5c48e052f7da5b0384
Alejandro Ruiz Rodríguez <ale_ruiz_rodriguez@hotmail.com>
2017-10-26 15:32:07 +0100

    some minor changes on DuFortFrankel and Analytical Solution

--- AnalyticalSolution.cpp
+++ AnalyticalSolution.cpp
@@ -8,10 +8,10 @@ AnalyticalSolution::AnalyticalSolution(std::vector<double> v) {
 }
 
 void AnalyticalSolution::compute() {
-	std::vector<double> t0aAnSol(n);
+	std::vector<double> t0aAnSol(n+1);
 
 	t0aAnSol[0] = 300.0;
-	t0aAnSol[n - 1] = 300.0;
+	t0aAnSol[n] = 300.0;
 
 	for (int i = 1; i < t0aAnSol.size() - 1; i++)
 	{
@@ -20,20 +20,20 @@ void AnalyticalSolution::compute() {
 
 	addToAllSolutions(t0aAnSol);
 
-	for (int j = 0; j < 48; j++) {
+	for (int j = 1; j < 51; j++) {
 
-		std::vector<double> anSol(n);
+		std::vector<double> anSol(n+1);
 		anSol[0] = 300.0;
-		anSol[n - 1] = 300.0;
+		anSol[n] = 300.0;
 		for (int i = 1; i < anSol.size() - 1; i++) {
 			double sum = 0.0;
 			for (int m = 1; m < 1000; m++)
 			{
-				sum += exp(-D*pow(((m*PI) / L), 2)*(deltaT*(j + 1))) * ((1 - pow((-1), m)) / (m*PI)) * sin((m*PI*(deltaX*i)) / L);
+				sum += exp(-D*pow(((m*PI) / L), 2)*(deltaT*(j))) * ((1 - pow((-1), m)) / (m*PI)) * sin((m*PI*(deltaX*i)) / L);
 			}
 			anSol[i] = 300 + 2 * (100 - 300) * sum;
 		}
-
-		addToAllSolutions(anSol);
+		if ((j % 10) == 0)
+			addToAllSolutions(anSol);
 	}
 } 
--- DuFortFrankelMethod.cpp
+++ DuFortFrankelMethod.cpp
@@ -7,21 +7,21 @@ DuFortFrankelMethod::DuFortFrankelMethod(std::vector<double> v) {
 }
 
 void DuFortFrankelMethod::compute() {
-	std::vector<double> t0Sol(n);
-	for (int i = 0; i < n; i++) {
-		if (i == 0 || i == (n - 1))
+	std::vector<double> t0Sol(n+1);
+	for (int i = 0; i < (n+1); i++) {
+		if (i == 0 || i == n)
 			t0Sol[i] = 300.0;
 		else
 			t0Sol[i] = 100.0;
 	}
 
-	std::vector<double> t1Sol(n);// We use the forward time, central space method to calculate the second timestep
+	std::vector<double> t1Sol(n+1);// We use the forward time, central space method to calculate the second timestep
 								//as we need it to use the DuFortFrankelMEthod. This method is stable in our case
 								// because (D*deltaT)/deltaX^2 = 0.4 (which is <= 0.5 which is the limit for stability)
 
 
-	for (int i = 0; i < n; i++) {
-		if (i == 0 || i == (n - 1))
+	for (int i = 0; i < (n + 1); i++) {
+		if (i == 0 || i == n)
 			t1Sol[i] = 300.0;
 		else
 			t1Sol[i] = t0Sol[i] + ((D*deltaT) / (pow(deltaX, 2)))
@@ -29,13 +29,12 @@ void DuFortFrankelMethod::compute() {
 	}
 
 	addToAllSolutions(t0Sol);
-	addToAllSolutions(t1Sol);
 	
-	for (int j = 0; j < 47; j++) {
+	for (int j = 0; j < 51; j++) {
 		
-		std::vector<double> compSol(n);
+		std::vector<double> compSol(n+1);
 		compSol[0] = 300.0;
-		compSol[n-1] = 300.0;
+		compSol[n] = 300.0;
 		for (int i = 1; i < compSol.size() - 1; i++) {
 			if (j == 0) {
 				compSol[i] = ((1 - ((2 * D*deltaT) / (pow(deltaX, 2)))) * t0Sol[i]
